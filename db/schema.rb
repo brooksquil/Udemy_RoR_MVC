99e9da51b2a345bf2b08a148d46df5f2fc3bbbf4
Brooke Wittenberg <bwittenberg.developer@gmail.com>
2021-10-03 13:47:45 -0500

    add articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_193753) do
+ActiveRecord::Schema.define(version: 2021_10_03_184400) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
